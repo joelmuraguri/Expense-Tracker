81e997ade27746cecfbfec3cbca1553b965d6131
joelmuraguri <joelkmuraguri21@gmail.com>
2023-07-08 18:58:11 +0300

    fix text align in topbar by using  centerAlignedAlignedTopBar

--- core/design/src/main/java/com/joel/design/components/ToolBar.kt
+++ core/design/src/main/java/com/joel/design/components/ToolBar.kt
@@ -1,14 +1,18 @@
 package com.joel.design.components
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.joel.design.R
 import com.joel.design.WalletIcons
 
@@ -18,7 +22,7 @@ fun NavigationToolBar(
     title : String,
     popBackStack : () -> Unit
 ){
-    TopAppBar(
+    CenterAlignedTopAppBar(
         title = {
             Text(
                 text = title,
@@ -28,7 +32,9 @@ fun NavigationToolBar(
             IconButton(onClick = { popBackStack() }) {
                 Icon(painter = painterResource(id = WalletIcons.ArrowBack), contentDescription = stringResource(id = R.string.arrow_back_icon_description))
             }
-        }
+        },
+        modifier = Modifier.padding(horizontal = 16.dp)
+
     )
     
 }
