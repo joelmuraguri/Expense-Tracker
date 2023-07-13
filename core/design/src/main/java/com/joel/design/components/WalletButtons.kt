c1084257447d762d7723c32b383d3277165d46b0
joelmuraguri <joelkmuraguri21@gmail.com>
2023-07-13 19:08:52 +0300

    create app auth buttons

---
+++ core/design/src/main/java/com/joel/design/components/WalletButtons.kt
@@ -0,0 +1,47 @@
+package com.joel.design.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SignInSignUpActionButton(
+    @StringRes text : Int
+){
+
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(70.dp)
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Text(text = stringResource(id = text))
+    }
+
+}
+
+@Composable
+fun GuestAndGoogleOutlinedButton(
+    @StringRes text : Int
+){
+
+    OutlinedButton(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .height(70.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Text(text = stringResource(id = text))
+    }
+
+}
